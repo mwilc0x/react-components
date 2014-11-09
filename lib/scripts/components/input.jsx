cf4cc7a8f92e0219ab33ad36aed3aff7c4841f61
Mike Wilcox <mwilcox56@gmail.com>
2014-11-08 22:28:23 -0500

    add external validations helper

--- lib/scripts/components/input.jsx
+++ lib/scripts/components/input.jsx
@@ -2,23 +2,17 @@
 
 var React = require('react/addons')
 var Input = require('react-bootstrap').Input;
+var Validations = require('../utils/validations');
 
 
 var InputComponent = React.createClass({
 
   mixins: [React.addons.LinkedStateMixin],
-  
+
   getInitialState: function() {
     return {
       value: ''
     };
-  },
-
-  validationState: function() {
-    var length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
   },
 
   render: function() {
@@ -28,7 +22,7 @@ var InputComponent = React.createClass({
           valueLink={this.linkState('value')}
           placeholder={this.props.placeholder}
           label={this.props.label}
-          bsStyle={this.validationState()}
+          bsStyle={Validations.length(this.state.value)}
           hasFeedback
           ref="input"
           groupClassName="group-class"
