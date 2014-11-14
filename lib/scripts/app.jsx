aeb568cb9ac49d7d2970cf7251b1d94e51895780
Mike Wilcox <mwilcox56@gmail.com>
2014-11-14 18:34:53 -0500

    modify main jsx file for todos

--- lib/scripts/app.jsx
+++ lib/scripts/app.jsx
@@ -1,22 +1,99 @@
-"use strict";
+var React = require('react');
+var Fluxxor = require('Fluxxor');
+var TodoStore = require('./stores/TodoStore');
+var actions = require('./actions/actions');
 
-var React = require('React');
-var Form = require('./components/form.jsx');
+var stores = {
+  TodoStore: new TodoStore()
+};
 
-var Page = React.createClass({
+var flux = new Fluxxor.Flux(stores, actions);
+
+window.flux = flux;
+
+flux.on("dispatch", function(type, payload) {
+  if (console && console.log) {
+    console.log("[Dispatch]", type, payload);
+  }
+});
+
+var FluxMixin = Fluxxor.FluxMixin(React),
+    StoreWatchMixin = Fluxxor.StoreWatchMixin;
+
+var Application = React.createClass({
+  mixins: [FluxMixin, StoreWatchMixin("TodoStore")],
 
   getInitialState: function() {
-    return {
+    return { newTodoText: "" };
+  },
 
+  getStateFromFlux: function() {
+    var flux = this.getFlux();
+    // Our entire state is made up of the TodoStore data. In a larger
+    // application, you will likely return data from multiple stores, e.g.:
+    //
+    //   return {
+    //     todoData: flux.store("TodoStore").getState(),
+    //     userData: flux.store("UserStore").getData(),
+    //     fooBarData: flux.store("FooBarStore").someMoreData()
+    //   };
+    return flux.store("TodoStore").getState();
+  },
+
+  render: function() {
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map(function(todo, i) {
+            return <li key={i}><TodoItem todo={todo} /></li>;
+          })}
+        </ul>
+        <form onSubmit={this.onSubmitForm}>
+          <input type="text" size="30" placeholder="New Todo"
+                 value={this.state.newTodoText}
+                 onChange={this.handleTodoTextChange} />
+          <input type="submit" value="Add Todo" />
+        </form>
+        <button onClick={this.clearCompletedTodos}>Clear Completed</button>
+      </div>
+    );
+  },
+
+  handleTodoTextChange: function(e) {
+    this.setState({newTodoText: e.target.value});
+  },
+
+  onSubmitForm: function(e) {
+    e.preventDefault();
+    if (this.state.newTodoText.trim()) {
+      this.getFlux().actions.addTodo(this.state.newTodoText);
+      this.setState({newTodoText: ""});
     }
   },
 
-  render: function(){
-    return (
-      <Form></Form>
-    );
+  clearCompletedTodos: function(e) {
+    this.getFlux().actions.clearTodos();
   }
-
 });
 
-React.render(<Page />, document.getElementById('content'));
+var TodoItem = React.createClass({
+  mixins: [FluxMixin],
+
+  propTypes: {
+    todo: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    var style = {
+      textDecoration: this.props.todo.complete ? "line-through" : ""
+    };
+
+    return <span style={style} onClick={this.onClick}>{this.props.todo.text}</span>;
+  },
+
+  onClick: function() {
+    this.getFlux().actions.toggleTodo(this.props.todo);
+  }
+});
+
+React.render(<Application flux={flux} />, document.getElementById("content"));
