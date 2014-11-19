f6d9388af0ac759dd5031c4a61f710898e1c88ac
Mike Wilcox <mwilcox56@gmail.com>
2014-11-18 20:14:06 -0500

    add basic server code

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+  http = require('http'),
+  mongoose = require('mongoose'),
+  socketio = require('socket.io'),
+  app = express(),
+  port = process.env.PORT || 8080;
+
+mongoose.connect('mongodb://localhost/react-components');
+
+app.use("/", express.static(__dirname + "/public/"));
+
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
+var io = socketio.listen(server);
