6beeeb39dcaaa998a2f147fc4d4c3c2bfc3cb3ec
Mike Wilcox <mwilcox56@gmail.com>
2014-11-22 19:29:13 -0500

    keep it simple

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ var express = require('express'),
   mongoose = require('mongoose'),
   socketio = require('socket.io'),
   app = express(),
-  port = 8080;
+  port = 8080,
+  uriString = 'mongodb://localhost/react-components';
 
-mongoose.connect('mongodb://localhost/react-components');
+mongoose.connect(uriString, function(err, res) {
+  if(err) {
+    console.log('error connecting to ' + uriString + '. ' + err);
+  } else {
+    console.log('success connecting to ' + uriString + '.');
+  }
+});
 
 app.use("/", express.static(__dirname + "/public/"));
 
 var server = http.createServer(app).listen(port, function() {
-  console.log('Express server listening on port ' + port);
+  console.log('express server listening on port ' + port);
 });
 
 var io = socketio.listen(server);
+
+//loadTodos();
